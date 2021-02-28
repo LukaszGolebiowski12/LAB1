c421b4cd187d95cf5f5c675a2c27e7403ca80dd0
Lukasz Golebiowski <lukasz.golebiowski321@wp.pl>
2021-02-28 16:21:36 +0100

    New classes

---
+++ src/pl/gda/wsb/Human.java
@@ -0,0 +1,13 @@
+package pl.gda.wsb;
+
+public class Human {
+    String name;
+    double age;
+    String sex;
+
+    public Human(String name, double age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+}
---
+++ src/pl/gda/wsb/Phone.java
@@ -0,0 +1,12 @@
+package pl.gda.wsb;
+
+public class Phone {
+
+    String model;
+    double weight;
+
+    public Phone(String model, double weight) {
+        this.model = model;
+        this.weight = weight;
+    }
+}
